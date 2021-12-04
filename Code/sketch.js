a71dc21c2cbbe69faa7af3f24f76bf9f6c6c2d34
Sudazzz <90943035+Sudazzz@users.noreply.github.com>
2021-12-03 19:49:28 -0500

    Second Draft

--- Code/sketch.js
+++ Code/sketch.js
@@ -10,24 +10,30 @@ function setup() {
     video.size(w, h);
     //video.hide();
   //button=createButton('snap');
-  //button.mousePressed(takesnap);
+var video;
+var button
+var w = 800;
+var h = 800;
+
+function setup() {
+    video = createCapture({
+        audio: false,
+        video: {
+            width: w,
+            height: h
+        }
+    }, function() {
+        console.log('capture ready.')
+    });
+    video.elt.setAttribute('playsinline', '');
+    video.size(w, h);
+    createCanvas(w, h);
+    //video.hide();
+  button=createButton('snap');
+  button.mousePressed(takesnap);
 }
 
 function draw() {
-  snapshots.push(video.get());
-  var a = 80;
-  var b = 60;
-  var x = 0;
-  var y = 0;
-  for(var i = 0; i < snapshots.length; i++){
-  imgae(snapshots[i],x,y,a,b);
-    x=x+a;
-    if(x > width){
-      x = 0;
-      y = y + b;
-    }
-  }
-  /*
   tint(200,200,100);
     image(video, 0, 0, w/2, h/2);
   tint(100,200,200);
@@ -39,20 +45,9 @@ function draw() {
     video.loadPixels();
     if (video.pixels.length > 0) { // don't forget this!
     }
-    */
-}
-
-var go = false;
-
-function ready(){
-  go = true;
 }
 
 function takesnap(){
-  if(go){
-  snapshots.push(video.get());
-  }
-  /*
    tint(200,200,100);
     image(video, 0, 0, w/2, h/2);
   tint(100,200,200);
@@ -61,5 +56,5 @@ function takesnap(){
   image(video, 400, 0, w/2, h/2);
   tint(200,200,200);
   image(video, 0, 400, w/2, h/2);
-  */
+  
 }
